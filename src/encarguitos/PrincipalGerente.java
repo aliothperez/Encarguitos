884c4edcb408ff080e48bab7f7a7e9beb6fb0dba
falio <falio@DESKTOP-H8K22J8>
2025-05-28 01:37:44 -0700

    Gerente

--- src/encarguitos/PrincipalGerente.java
+++ src/encarguitos/PrincipalGerente.java
@@ -28,7 +28,6 @@ public class PrincipalGerente extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         BtnVolver = new javax.swing.JButton();
-        BtnServicios = new javax.swing.JButton();
         BtnUsuarios = new javax.swing.JButton();
         BtnClientes = new javax.swing.JButton();
         BtnPedidos = new javax.swing.JButton();
@@ -50,15 +49,6 @@ public class PrincipalGerente extends javax.swing.JFrame {
         BtnVolver.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jPanel1.add(BtnVolver, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 30, -1, -1));
 
-        BtnServicios.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        BtnServicios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Servicios.png"))); // NOI18N
-        BtnServicios.setText("Servicios");
-        BtnServicios.setContentAreaFilled(false);
-        BtnServicios.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        BtnServicios.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        BtnServicios.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jPanel1.add(BtnServicios, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 130, 90, 90));
-
         BtnUsuarios.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         BtnUsuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/usuario.png"))); // NOI18N
         BtnUsuarios.setText("Usuarios");
@@ -67,7 +57,7 @@ public class PrincipalGerente extends javax.swing.JFrame {
         BtnUsuarios.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BtnUsuarios.setVerifyInputWhenFocusTarget(false);
         BtnUsuarios.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jPanel1.add(BtnUsuarios, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 110, -1, -1));
+        jPanel1.add(BtnUsuarios, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
 
         BtnClientes.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         BtnClientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/cliente.png"))); // NOI18N
@@ -76,7 +66,7 @@ public class PrincipalGerente extends javax.swing.JFrame {
         BtnClientes.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         BtnClientes.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BtnClientes.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jPanel1.add(BtnClientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 100, 90, -1));
+        jPanel1.add(BtnClientes, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 100, 90, -1));
 
         BtnPedidos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         BtnPedidos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Pedido.png"))); // NOI18N
@@ -85,7 +75,7 @@ public class PrincipalGerente extends javax.swing.JFrame {
         BtnPedidos.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         BtnPedidos.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BtnPedidos.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jPanel1.add(BtnPedidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 130, -1, 90));
+        jPanel1.add(BtnPedidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 130, -1, 90));
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/LogoCarro.png"))); // NOI18N
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(-10, 480, 120, 50));
@@ -159,7 +149,6 @@ public class PrincipalGerente extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnClientes;
     private javax.swing.JButton BtnPedidos;
-    private javax.swing.JButton BtnServicios;
     private javax.swing.JButton BtnUsuarios;
     private javax.swing.JButton BtnVolver;
     private javax.swing.JLabel jLabel1;
